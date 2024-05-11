54c2d01e1a0efe4a745db415cfcfde002c08a4ce
tappei-noda <pitaaren@gmail.com>
2024-05-11 07:19:01 +0000

    fix data

--- routes/v1/friDayMovie.js
+++ routes/v1/friDayMovie.js
@@ -24,13 +24,13 @@ function checkBroadcastDate(dateString,titleString) {
     const broadcastDate = dateString.split('放送')[0]; // 日付部分を取得する
     // 今日の日付を取得する
     const today = new Date();
-    // const todayFormatted = today.getFullYear() + '.' + (today.getMonth() + 1) + '.' + today.getDate();
-    const todayFormatted = "2024.4.26"
+    const todayFormatted = today.getFullYear() + '.' + (today.getMonth() + 1) + '.' + today.getDate();
+    //const todayFormatted = "2024.4.26"
     // 今日の日付と比較する
     if (broadcastDate === todayFormatted) {
         return {date:broadcastDate, title:titleString}; // 日付が一致する場合
     } else {
-        return {date:"", title:"放送お休み"}; // 日付が一致しない場合
+        return {date:todayFormatted, title:broadcastDate}; // 日付が一致しない場合
     }
 }
 
